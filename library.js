1e275ed4b4fad221b487441fa45590c040c71ff1
Muskan Agarwal <muskanagarwal3110@gmail.com>
2024-02-13 23:25:12 +0530

    files

---
+++ library.js
@@ -0,0 +1,15 @@
+const subscribeBtn = document.getElementById('subscribeBtn');
+const subscribeStatus = document.getElementById('subscribeStatus');
+
+subscribeBtn.addEventListener('click', function() {
+  subscribeStatus.textContent = 'You are now subscribed!';
+});
+
+function toggleElement() {
+    const element = document.getElementById('elementToToggle');
+    if (element.style.display === 'none') {
+      element.style.display = 'block'; // Or 'inline', 'flex', etc. based on your layout needs
+    } else {
+      element.style.display = 'none';
+    }
+  }
---
+++ log.js
@@ -0,0 +1,17 @@
+function toggleElement() {
+  const element = document.getElementById('sort_ToToggle');
+  if (element.style.display === 'none') {
+    element.style.display = 'block'; 
+  } else {
+    element.style.display = 'none';
+  }
+}
+
+function ListElement() {
+  const element = document.getElementById('List_ToToggle');
+  if (element.style.display === 'none') {
+    element.style.display = 'block'; 
+  } else {
+    element.style.display = 'none';
+  }
+}
